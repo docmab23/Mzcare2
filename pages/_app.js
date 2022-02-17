1f637973a1e986fa3f26080be81d9259de7b37c6
unknown <docmab23@gmail.com>
2022-02-16 23:26:37 -0500

    here

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import { ethers } from 'ethers'
+import {useEffect , useState } from 'react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
+
+import {
+  nftaddress , nftmarketaddress}
+from "../config"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Home</h1>
+      </div>
+
+
+
+  
+  )
+}
+
