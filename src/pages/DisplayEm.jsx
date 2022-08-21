b71bd58f59f65dcf52a24346ff9f5d11419b4f37
tejas bhuwania <tejasbhuwania2805@gmail.com>
2022-08-21 18:15:45 -0400

    ko

--- src/pages/DisplayEm.jsx
+++ src/pages/DisplayEm.jsx
@@ -35,6 +35,7 @@ import General from "./resource-pages/General";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { useLocation } from "react-router";
+import FormTopBarW from "../components/FormTopBarW";
 
 function DisplayEm() {
   var show_ = false;
@@ -49,7 +50,7 @@ function DisplayEm() {
   const location = useLocation();
   var uid = location.pathname;
   uid = uid.split("/").pop();
-  const [address, setAddress] = useState([]);
+  const [address, setAddress] = useState("");
   const [immunizationJson, setImmunizationJson] = useState("")
   const [immunizationList, setImmunizationList] = useState([])
   const [allergyJson, setAllergyJson] = useState("")
@@ -62,9 +63,10 @@ function DisplayEm() {
   const [genList, setGenList] = useState([])
 
   function getData() {
+    getLocation();
+    getICE();
     getImmunizations();
     getAllergy();
-    getICE();
     getCondition();
     getGeneral();
   }
@@ -144,7 +146,11 @@ function DisplayEm() {
     })
   }
 
-  const getLocation = async () => {
+  useEffect(() => {
+    Sms_Ice();
+  }, [iceList, address]);
+
+  async function getLocation() {
     try {
       const position = await Geolocation.getCurrentPosition();
       const lat = position.coords.latitude.toString();
@@ -153,6 +159,7 @@ function DisplayEm() {
     } catch (e) {
       toast(e);
     }
+    return "ok"
   };
 
   async function Send_Sms(number, pos_) {
@@ -161,139 +168,149 @@ function DisplayEm() {
       redirect: "follow",
     };
 
-    fetch(
-      `http://mzcare2.herokuapp.com/api/messages?to="${number}"&body="There's an emergency at:"${pos_}""`,
-      requestOptions
-    )
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
+    if (address!== undefined && address !== "") {
+      fetch(
+        `http://mzcare2.herokuapp.com/api/messages?to="${number}"&body="There's an emergency at:"${pos_}""`,
+        requestOptions
+      )
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    }
   }
 
   async function Sms_Ice() {
     iceList.map((item, pos) => {
       const phone_no = iceJson[item]["number"];
-      getLocation().then(Send_Sms(phone_no, address));
+        Send_Sms(phone_no, address);
     });
-  }
-
-  if (iceList.length != 0) {
-    Sms_Ice();
   }
 
   return (
     <IonPage>
-      <IonContent className="ion-padding">
-        <FormTopBar />
-        {"\u00a0\u00a0\u00a0"}
-        <h1> {"\u00a0\u00a0\u00a0"}</h1>
-        <h1> {"\u00a0\u00a0\u00a0"}</h1>
-        <IonTitle align="center" color="primary">
-          MzCare Emergency Profile{" "}
-        </IonTitle>
-        <IonCard>
-          <IonCardContent>
-            {genList.map((item, pos) => {
-              return (
-                <IonGrid>
-                  <IonRow key={pos}>
-                    <IonCol>
-                      <IonText color="primary">
-                        Name: {genJson[item]["Name"]}
-                      </IonText>
-                    </IonCol>
-                    <IonCol>
-                      <IonText color="primary">Age</IonText>:{" "}
-                      {genJson[item]["Age"]}
-                    </IonCol>
-                    <IonCol>
-                      <IonText color="primary">Blood Group</IonText>:{" "}
-                      {genJson[item]["Bloodgroup"]}
-                    </IonCol>
-                    <IonCol>
-                      <IonText color="primary">Full Address</IonText>:{" "}
-                      {genJson[item]["Str_address"] +
-                        ", " +
-                        genJson[item]["City"] +
-                        ", " +
-                        genJson[item]["State"] +
-                        ", " +
-                        genJson[item]["Zip"]}
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
-              );
-            })}
-          </IonCardContent>
-        </IonCard>
-        <h2>ICE contacts</h2>
-        <IonCard>
-          <IonCardContent>
-            {iceList.map((item, pos) => {
-              return (
-                <IonGrid>
-                  <IonRow key={pos}>
-                    <IonCol>
-                      <IonText color="primary" font="bold">
-                        Contact Name:
-                      </IonText>
-                      <div>{iceJson[item]["name"]}</div>
-                    </IonCol>
-                    <IonCol>
-                      <IonText color="primary" font="bold">
-                        Contact No:
-                      </IonText>
-                      <div>{iceJson[item]["number"]}</div>
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
-              );
-            })}
-          </IonCardContent>
-        </IonCard>
-        <h2>Allergies</h2>
-        <IonCard>
-          <IonCardContent>
-            {allergyList.map((item, pos) => {
-              return (
-                <IonGrid key={pos}>
-                  <IonRow>
-                    <IonText color="primary">Source:</IonText>
-                    {allergyJson[item]["allergyName"]}
-                  </IonRow>
-                </IonGrid>
-              );
-            })}
-          </IonCardContent>
-        </IonCard>
+    <IonContent className="ion-padding" color="primary">
+      <FormTopBarW />
+  <IonGrid align="center" color="primary">
+  <IonRow>
+        <IonText>MzCare Emergency Profile{" "}</IonText>
+  </IonRow>
+      </IonGrid>
+      {"\u00a0\u00a0\u00a0"}
+      <h1> {"\u00a0\u00a0\u00a0"}</h1>
+      <h1> {"\u00a0\u00a0\u00a0"}</h1>
+      
+      <IonCard>
+        <IonCardContent>
+          {genList.map((item, pos) => {
+            return (
+          
+                  <>
+                   <IonRow><IonText color="primary">
+                      Name {"\u00a0\u00a0"}
+                    </IonText>
+        <IonText >{genJson[item]["Name"]}</IonText></IonRow>
+        <IonRow>
+        <IonText color="primary">
+                      Age {"\u00a0\u00a0"}
+                    </IonText>
+        <IonText >{genJson[item]["Age"]}</IonText>
+        </IonRow>
+        <IonRow>
+        <IonText color="primary">
+                      Blood Group {"\u00a0\u00a0"}
+                    </IonText>
+        <IonText >{genJson[item]["Bloodgroup"]}</IonText>
+        </IonRow>
+        <IonRow>
+        <IonText color="primary">
+                      Full Address {"\u00a0\u00a0"}
+                    </IonText>
+        <IonText> {genJson[item]["Str_address"] +
+                      ", " +
+                      genJson[item]["City"] +
+                      ", " +
+                      genJson[item]["State"] +
+                      ", " +
+                      genJson[item]["Zip"]}</IonText>
+          </IonRow>
+    
+             </>
+            );
+          })}
+        </IonCardContent>
+      </IonCard>
+      <h2>ICE contacts</h2>
+      <IonCard>
+        <IonCardContent>
+          {iceList.map((item, pos) => {
+            return (
+              <IonGrid>
+                <IonRow key={pos}>
+                  <IonCol>
+                    <IonText color="primary" font="bold">
+                      Name  {"\u00a0\u00a0"}
+                    </IonText>
+                    <div>{iceJson[item]["name"]}</div>
+                  </IonCol>
+                  <IonCol>
+                    <IonText color="primary" font="bold">
+                     Contact No {"\u00a0\u00a0"}
+                    </IonText>
+                    <div>{iceJson[item]["number"]}</div>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            );
+          })}
+        </IonCardContent>
+      </IonCard>
+      <h2>Allergies</h2>
+      <IonCard>
+        <IonCardContent>
+          {allergyList.map((item, pos) => {
+            return (
+              <IonGrid key={pos}>
+                <IonRow>
+                  <IonText color="primary">Source {"\u00a0\u00a0"}</IonText>
+                  {allergyJson[item]["allergyName"]}
+                </IonRow>
+              </IonGrid>
+            );
+          })}
+        </IonCardContent>
+      </IonCard>
 
-        <h2>Immunizations</h2>
-        <IonCard>
-          <IonCardContent>
-            {immunizationList.map((item, pos) => {
-              return (
-                <IonGrid key={pos}>
-                  <IonRow>{immunizationJson[item]["vaccineName"]}</IonRow>
-                </IonGrid>
-              );
-            })}
-          </IonCardContent>
-        </IonCard>
+      <h2>Immunizations</h2>
+      <IonCard>
+        <IonCardContent>
+          {immunizationList.map((item, pos) => {
+            return (
+              <IonGrid key={pos}>
+                <IonRow>
+                  {immunizationJson[item]["vaccineName"]}
+                </IonRow>
+              </IonGrid>
+            );
+          })}
+        </IonCardContent>
+      </IonCard>
 
-        <h2>Conditions</h2>
-        <IonCard>
-          <IonCardContent>
-            {conditionList.map((item, pos) => {
-              return (
-                <IonGrid key={pos}>
-                  <IonRow>{conditionJson[item]["conditionName"]}</IonRow>
-                </IonGrid>
-              );
-            })}
-          </IonCardContent>
-        </IonCard>
-      </IonContent>
-    </IonPage>
+      <h2>Conditions</h2>
+      <IonCard>
+        <IonCardContent>
+          {conditionList.map((item, pos) => {
+            return (
+              <IonGrid key={pos}>
+                <IonRow>
+                  {conditionJson[item]["conditionName"]}
+                </IonRow>
+              </IonGrid>
+            );
+          })}
+        </IonCardContent>
+      </IonCard>
+    </IonContent>
+  </IonPage>
   );
 }
 
